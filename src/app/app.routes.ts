7cd141597e6415713f6b2190097f32aed0bdf3c0
Johnni5 <johnni5@proton.me>
2024-08-29 18:23:26 +0200

    "register-user component -ready, Form-Validation - pending, font:figtree installed"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainContentComponent } from './main-content/main-content/main-content.c
 import { MenuSideLeftComponent } from './main-content/menu-side-left/menu-side-left/menu-side-left.component';
 import { LegalComponent } from './legal/legal.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 export const routes: Routes = [
   { path: 'main', component: MainContentComponent },
   { path: '', component: MenuSideLeftComponent },
   { path: 'legal', component: LegalComponent },
   { path: 'imprint', component: ImprintComponent },
+  { path: 'register', component: RegisterUserComponent },
 ];
