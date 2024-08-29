903b1273dd6735133e1b10b5f5441c83cb4c6391
Johnni5 <johnni5@proton.me>
2024-08-29 14:34:10 +0200

    "favicon installed, login-page (html) ready, imprint+legal -start"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content/main-content.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content/main-content.component';
 import { MenuSideLeftComponent } from './main-content/menu-side-left/menu-side-left/menu-side-left.component';
+import { LegalComponent } from './legal/legal.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
   { path: 'main', component: MainContentComponent },
   { path: '', component: MenuSideLeftComponent },
+  { path: 'legal', component: LegalComponent },
+  { path: 'imprint', component: ImprintComponent },
 ];
