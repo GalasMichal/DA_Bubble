ec14c6e2bb5061c128c6631e61220e59d581b6b3
Johnni5 <johnni5@proton.me>
2024-08-30 13:33:03 +0200

    "edited some scss"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from "../shared/header/header.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, HeaderComponent],
   templateUrl: './login.component.html',
   styleUrls: [
     './login.component.scss',
