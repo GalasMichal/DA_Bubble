2cad46b6687d1be0a26e425dc49b18bfac86a03f
Johnni5 <johnni5@proton.me>
2024-08-30 19:06:41 +0200

    "created copmonent"

---
+++ src/app/create-avatar/create-avatar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-avatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-avatar.component.html',
+  styleUrls: [
+    './create-avatar.component.scss',
+    './create-avatar.component.media.scss'
+  ]
+})
+
+export class CreateAvatarComponent {
+
+}
