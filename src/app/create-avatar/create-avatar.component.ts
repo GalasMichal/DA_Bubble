6ded607b77d10e4afd6332facc809836c07bdccc
Johnni5 <johnni5@proton.me>
2024-08-30 22:12:02 +0200

    "create-avatar final, anbindung upload pending"

--- src/app/create-avatar/create-avatar.component.ts
+++ src/app/create-avatar/create-avatar.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-create-avatar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './create-avatar.component.html',
   styleUrls: [
     './create-avatar.component.scss',
