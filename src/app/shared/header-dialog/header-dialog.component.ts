8e9faa0a560581817e9f4654def9076a33324428
Damian Poloczek <damianpoloczek@gmail.com>
2024-08-30 19:39:44 +0200

    new header-dialog.component

---
+++ src/app/shared/header-dialog/header-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-dialog.component.html',
+  styleUrl: './header-dialog.component.scss'
+})
+export class HeaderDialogComponent {
+
+}
