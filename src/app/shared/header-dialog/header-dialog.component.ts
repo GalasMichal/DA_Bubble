7e1eca719a6786e0ba5b91f39e8515ced27e732a
Damian Poloczek <damianpoloczek@gmail.com>
2024-08-31 20:25:54 +0200

    profile structure ready

--- src/app/shared/header-dialog/header-dialog.component.ts
+++ src/app/shared/header-dialog/header-dialog.component.ts
@@ -11,17 +11,17 @@ import { ProfileComponent } from '../profile/profile.component';
 @Component({
   selector: 'app-header-dialog',
   standalone: true,
-  imports: [MatButtonModule, MatDialogContent, ProfileComponent],
+  imports: [MatButtonModule, MatDialogContent, ProfileComponent, MatButtonModule],
   templateUrl: './header-dialog.component.html',
   styleUrl: './header-dialog.component.scss'
 })
 export class HeaderDialogComponent {
   dialog = inject(MatDialog)
 
-  openDialog() {
-    this.dialog.open(ProfileComponent), {
-      panelClass: 'profile-container',
-    }
+  openDialogProfile() {
+    this.dialog.open(ProfileComponent, {
+      panelClass: 'profile-container', // Custom class for profile dialog
+    });
   }
 
 }
--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { MatDialog, MatDialogContent } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatDialog, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [MatDialogContent],
+  imports: [MatDialogContent, MatButtonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  dialog = inject(MatDialogRef <ProfileComponent>)
+
+  closeDialogProfile() {
+    this.dialog.close()
+  }
 
 }
