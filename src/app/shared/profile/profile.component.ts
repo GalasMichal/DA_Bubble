7b4f7d95d8f0ca25febeca6aec3e06ea765d6c3f
Damian Poloczek <damianpoloczek@gmail.com>
2024-08-31 17:56:17 +0200

    profil structure

---
+++ src/app/shared/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogContent } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-profil',
+  standalone: true,
+  imports: [MatDialogContent],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+
+}
