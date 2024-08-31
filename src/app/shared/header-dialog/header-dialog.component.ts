e2290d3cbfb32854f38d5f7c84cc6007e4e7969e
Damian Poloczek <damianpoloczek@gmail.com>
2024-08-31 13:10:08 +0200

    struktur ready

--- src/app/shared/header-dialog/header-dialog.component.ts
+++ src/app/shared/header-dialog/header-dialog.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-header-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogContent],
   templateUrl: './header-dialog.component.html',
   styleUrl: './header-dialog.component.scss'
 })
