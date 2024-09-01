7bbbe32084ba5b74def8ffc497271047e754d9b9
Damian Poloczek <damianpoloczek@gmail.com>
2024-09-01 11:30:33 +0200

    edit-profile structure ready

--- src/app/shared/edit-profile/edit-profile.component.ts
+++ src/app/shared/edit-profile/edit-profile.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogRef
+} from '@angular/material/dialog';
+import { MatButtonModule, } from '@angular/material/button';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [],
+  imports: [MatDialogContent, MatButtonModule],
   templateUrl: './edit-profile.component.html',
-  styleUrl: './edit-profile.component.scss'
+  styleUrl: './edit-profile.component.scss',
 })
 export class EditProfileComponent {
+  readonly dialog = inject(MatDialogRef <EditProfileComponent>)
 
+  closeDialogEdit() {
+    this.dialog.close()
+  }
 }
--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -1,19 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { MatDialog, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import { EditProfileComponent } from '../edit-profile/edit-profile.component';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [MatDialogContent, MatButtonModule],
+  imports: [MatDialogContent, MatButtonModule, EditProfileComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   readonly dialog = inject(MatDialogRef <ProfileComponent>)
+  readonly editDialog = inject(MatDialog)
 
   closeDialogProfile() {
     this.dialog.close()
+  }
+
+  openDialogEdit() {
+    this.editDialog.open(EditProfileComponent, {
+      panelClass: 'profile-container', // Custom class for profile dialog
+    });
   }
 
 }
