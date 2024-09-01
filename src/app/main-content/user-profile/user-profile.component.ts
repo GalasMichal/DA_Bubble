3ef7ba0664c2cd92e23aa6d159ede32bb9557d01
Damian Poloczek <damianpoloczek@gmail.com>
2024-09-01 22:01:21 +0200

    add user-profile to main-content

--- src/app/main-content/user-profile/user-profile.component.ts
+++ src/app/main-content/user-profile/user-profile.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [MatDialogContent, MatButtonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent {
+  readonly dialog = inject(MatDialogRef <UserProfileComponent>)
+
+  closeUserProfile() {
+    this.dialog.close()
+  }
 
 }
