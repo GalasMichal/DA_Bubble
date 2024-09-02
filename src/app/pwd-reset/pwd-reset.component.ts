10f2981e717d683d5080f76ae05534252c7ea829
Johnni5 <johnni5@proton.me>
2024-09-02 15:04:49 +0200

    "pwdResetPage - edited/fixed, routing, font, with-OPEN"

--- src/app/pwd-reset/pwd-reset.component.ts
+++ src/app/pwd-reset/pwd-reset.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pwd-reset',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, RouterLink],
   templateUrl: './pwd-reset.component.html',
   styleUrl: './pwd-reset.component.scss'
 })
