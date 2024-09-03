0ef992f6446b484931aa3c6c9eb7681a487b8bff
Michal Galas <michalgalas1989@gmail.com>
2024-09-03 13:52:29 +0200

    replace sidenav to component, menu finished

--- src/app/main-content/main-content/main-content.component.ts
+++ src/app/main-content/main-content/main-content.component.ts
@@ -1,43 +1,31 @@
-import { Component, ComponentFactoryResolver, inject, ViewChild, ViewContainerRef } from '@angular/core';
-
-
-
-
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { LogoComponent } from '../../logo/logo.component';
-import { CreateAvatarComponent } from '../../create-avatar/create-avatar.component';
+
+import { MenuSideLeftComponent } from '../menu-side-left/menu-side-left/menu-side-left.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, LogoComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    LogoComponent,
+    MenuSideLeftComponent,
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
 export class MainContentComponent {
-  readonly userDialog = inject(MatDialog)
-
-  isMenuOpen= true;
-  isFirstDropdownMenuOpen = false ;
-  isSecondDropdownMenuOpen = true;
-
-
-  toogleDropDown1(){
-    this.isFirstDropdownMenuOpen = !this.isFirstDropdownMenuOpen;
-  }
-
-  toogleDropDown2(){
-    this.isSecondDropdownMenuOpen = !this.isSecondDropdownMenuOpen
-  }
+  readonly userDialog = inject(MatDialog);
+  isMenuOpen = true;
 
   openUserProfile() {
     this.userDialog.open(UserProfileComponent, {
-      panelClass: 'user-profile-container'
-    })
+      panelClass: 'user-profile-container',
+    });
   }
-
 }
-
