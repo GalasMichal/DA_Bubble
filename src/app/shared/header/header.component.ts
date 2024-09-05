8c072dc75f3c2404970dcd03f6f3483ff55b9913
Damian Poloczek <damianpoloczek@gmail.com>
2024-09-05 21:03:10 +0200

    new component avatar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,11 +3,12 @@ import { HeaderDialogComponent } from '../header-dialog/header-dialog.component'
 import { MatDialog } from '@angular/material/dialog';
 import { LogoComponent } from '../logo/logo.component';
 import { SearchComponent } from '../search/search.component';
+import { AvatarComponent } from '../avatar/avatar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderDialogComponent, LogoComponent, SearchComponent],
+  imports: [HeaderDialogComponent, LogoComponent, SearchComponent, AvatarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
