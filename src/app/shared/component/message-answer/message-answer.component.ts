8eea1584ec8811e0c640a827bccdb557dc4bd657
Damian Poloczek <damianpoloczek@gmail.com>
2024-09-10 23:42:28 +0200

    reaction-bar structure ready

--- src/app/shared/component/message-answer/message-answer.component.ts
+++ src/app/shared/component/message-answer/message-answer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ReactionBarComponent } from '../reaction-bar/reaction-bar.component';
 
 @Component({
   selector: 'app-message-answer',
   standalone: true,
-  imports: [],
+  imports: [ReactionBarComponent],
   templateUrl: './message-answer.component.html',
   styleUrl: './message-answer.component.scss'
 })
