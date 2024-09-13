0925028c7ab23a3ad9b7eb0d7312a36a608dda4e
Damian Poloczek <damianpoloczek@gmail.com>
2024-09-13 19:05:09 +0200

    developer to reaction-bar

--- src/app/shared/component/message-answer/message-answer.component.ts
+++ src/app/shared/component/message-answer/message-answer.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { ReactionBarComponent } from '../reaction-bar/reaction-bar.component';
 import { TimeSeparatorComponent } from './time-separator/time-separator.component';
 import { UserService } from '../../../services/user.service';
