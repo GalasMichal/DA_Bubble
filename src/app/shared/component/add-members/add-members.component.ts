023707adf4d8a97c1c232f274748e9ecc39a76ee
Damian Poloczek <damianpoloczek@gmail.com>
2024-09-15 09:08:37 +0200

    logik with radio button works

--- src/app/shared/component/add-members/add-members.component.ts
+++ src/app/shared/component/add-members/add-members.component.ts
@@ -1,27 +1,44 @@
 import { Component, inject } from '@angular/core';
 import { MatDialogContent, MatDialogRef } from '@angular/material/dialog';
-import {MAT_RADIO_DEFAULT_OPTIONS, MatRadioModule} from '@angular/material/radio';
 import { InputAddUsersComponent } from '../input-add-users/input-add-users.component';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-members',
   standalone: true,
-  imports: [MatDialogContent, MatRadioModule, InputAddUsersComponent, MatFormFieldModule, MatSelectModule, MatInputModule],
+  imports: [
+    MatDialogContent,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputAddUsersComponent,
+  ],
   templateUrl: './add-members.component.html',
   styleUrl: './add-members.component.scss',
-  providers: [{
-    provide: MAT_RADIO_DEFAULT_OPTIONS,
-    useValue: { color: 'red' },
-}]
 })
 export class AddMembersComponent {
-  dialog = inject(MatDialogRef <AddMembersComponent>)
+  dialog = inject(MatDialogRef<AddMembersComponent>);
+  selectedOption: string = '';
+  specificPeople: boolean = false;
+  allMembers: boolean = false
 
   closeDialogAddMembers() {
     this.dialog.close();
   }
 
+  choosenOption() {
+    if (this.selectedOption === 'specificPeople') {
+      this.specificPeople = true;
+    } else {
+      this.specificPeople = false
+      this.allMembers = true
+    }
+  }
 }
