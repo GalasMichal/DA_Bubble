37ca71a397709c296f19af841aaef708c2d64a30
Michal Galas <michalgalas1989@gmail.com>
2024-09-16 16:47:42 +0200

    changing name and id from project to bind correctly firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content/main-content.component';
-import { LoginComponent } from "./login/login.component";
+import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, LoginComponent, HeaderComponent, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainContentComponent,
+    LoginComponent,
+    HeaderComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'DA_Bubble';
+  title = 'dabubble';
+  firestore: Firestore = inject(Firestore)
 }
