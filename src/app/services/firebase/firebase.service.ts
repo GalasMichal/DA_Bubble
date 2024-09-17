4db03a45f753d4ecfc413b88e1ed23d8a7f14e16
Michal Galas <michalgalas1989@gmail.com>
2024-09-17 13:54:49 +0200

    added interfaces and services

--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -1,11 +1,29 @@
 import { inject, Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 import { Firestore } from '@angular/fire/firestore';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
-  firestore: Firestore = inject(Firestore)
+  firestore: Firestore = inject(Firestore);
+  private auth: Auth = inject(Auth);
+
   constructor() { }
+
+  // Methode zum Erstellen eines neuen Benutzers
+  createUser(email: string, password: string): Promise<any> {
+    return createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Erfolgreiche Anmeldung
+        const user = userCredential.user;
+        console.log('User created:', user);
+        return user;
+      })
+      .catch((error) => {
+        // Fehlerbehandlung
+        console.error('Error creating user:', error);
+        throw error;
+      });
+  }
 }
