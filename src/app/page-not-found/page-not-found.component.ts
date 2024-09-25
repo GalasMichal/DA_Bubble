2a75f43dd08c46e76869d7d49256aec7804ba890
Johnni5 <johnni5@proton.me>
2024-09-25 16:02:20 +0200

    "404-page created, working"

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../shared/header/header.component";
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { LoginComponent } from "../login/login.component";
 import { LogoComponent } from "../shared/logo/logo.component";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-page-not-found',
@@ -21,4 +21,11 @@ import { LogoComponent } from "../shared/logo/logo.component";
 })
 export class PageNotFoundComponent {
 
+  readonly location = inject(Location);
+
+  constructor() {}
+
+  goBack(): void {
+    this.location.back(); // Navigate to the previous page
+  }
 }
