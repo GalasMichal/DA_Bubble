2bdb5caf782aae1ac016476beb6b38a53e91e095
Damian Poloczek <damianpoloczek@gmail.com>
2024-10-16 23:25:33 +0200

    user added to input filed scss almost ready

--- src/app/shared/component/input-add-users/input-add-users.component.ts
+++ src/app/shared/component/input-add-users/input-add-users.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AvatarComponent } from '../../avatar/avatar.component';
 import { UserServiceService } from '../../../services/user-service/user-service.service';
+import { CloseComponent } from '../close/close.component';
 
 @Component({
   selector: 'app-input-add-users',
   standalone: true,
-  imports: [AvatarComponent],
+  imports: [AvatarComponent, CloseComponent],
   templateUrl: './input-add-users.component.html',
   styleUrl: './input-add-users.component.scss'
 })
@@ -13,10 +14,9 @@ export class InputAddUsersComponent {
   userService = inject(UserServiceService)
   choosenUser:string[] = []
 
-  addUser() {
-    console.log(this.userService);
-    
+  addUser(user: string) {
+    this.choosenUser.push(user)
+    console.log(this.choosenUser);
   }
-
 }
 
