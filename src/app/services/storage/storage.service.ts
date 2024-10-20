86561d617e9fcb074315cdf14da1b4ec6d46f0a5
Michal Galas <michalgalas1989@gmail.com>
2024-10-20 12:34:29 +0200

    added signal for user feedback

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -1,16 +1,17 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Storage } from '@angular/fire/storage';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
-
-  constructor() { }
+  constructor() {}
   private storage = inject(Storage);
+  uploadMsg = signal('eigenes Bild verwenden');
 
   uploadUserAvatar(event: any) {
-
+    const file = event.target.files[0];
+    this.uploadMsg.set(file.name);
+    console.log('file', file);
   }
-
 }
