2f4a21cfd578dae042e6594d11e47747033a2cc4
Michal Galas <michalgalas1989@gmail.com>
2024-10-20 10:32:09 +0200

    added firebase storage to Project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -32,6 +32,7 @@ export const appConfig: ApplicationConfig = {
         measurementId: 'G-WJY4TV84X3',
       })
     ),
+    provideStorage(() => getStorage()),
     provideAuth(() => getAuth()),
     provideAnalytics(() => getAnalytics()),
     ScreenTrackingService,
---
+++ src/app/services/storage/storage.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from '@angular/core';
+import { Storage } from '@angular/fire/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+  private storage = inject(Storage);
+
+  uploadUserAvatar
+
+}
