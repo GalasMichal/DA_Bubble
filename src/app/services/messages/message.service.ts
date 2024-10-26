f0e679ea6fa0e4ff5ee673e824b468c6b7d6a0e1
Michal Galas <michalgalas1989@gmail.com>
2024-10-26 11:16:47 +0200

    add PN collections to firebase

--- src/app/services/messages/message.service.ts
+++ src/app/services/messages/message.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { FirebaseService } from '../firebase/firebase.service';
+import { collection, doc, setDoc } from '@angular/fire/firestore';
+import { PrivateChat } from '../../models/interfaces/privateChat.class';
+import { UserServiceService } from '../user-service/user-service.service';
+import { User } from '../../models/interfaces/user.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  db = inject(FirebaseService)
+  user = inject(UserServiceService)
+
+ async newPrivateMessage(privateChat: PrivateChat, user: User) {
+  const channelCollectionRef = collection(this.db.firestore, 'privateMessages');
+  const channelDocRef = doc(channelCollectionRef);
+  privateChat.privatChatId = channelDocRef.id;
+  privateChat.chatCreator = this.db.currentUser()!.uId;
+  privateChat.chatReciver = user.uId;
+  await setDoc(channelDocRef, privateChat);
+ }
 
   constructor() { }
+
+
 }
+
+
