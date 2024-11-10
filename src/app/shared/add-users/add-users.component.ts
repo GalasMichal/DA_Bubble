2ee6f33ff9a44c2dc30de9246c8c8f8e3ef2e0b9
Michal Galas <michalgalas1989@gmail.com>
2024-11-10 22:03:44 +0100

    add specific people update in channel

--- src/app/shared/add-users/add-users.component.ts
+++ src/app/shared/add-users/add-users.component.ts
@@ -4,17 +4,19 @@ import { AvatarComponent } from '../avatar/avatar.component';
 import { InputAddUsersComponent } from '../component/input-add-users/input-add-users.component';
 import { ChatRoomService } from '../../services/chat-room/chat-room.service';
 import { CloseComponent } from '../component/close/close.component';
+import { StateControlService } from '../../services/state-control/state-control.service';
 
 @Component({
   selector: 'app-add-users',
   standalone: true,
-  imports: [MatDialogContent, AvatarComponent, InputAddUsersComponent, CloseComponent],
+  imports: [MatDialogContent, InputAddUsersComponent, CloseComponent],
   templateUrl: './add-users.component.html',
   styleUrl: './add-users.component.scss'
 })
 export class AddUsersComponent {
   readonly dialog = inject(MatDialogRef <AddUsersComponent>)
-  chat = inject(ChatRoomService)
+  chat = inject(ChatRoomService);
+  state = inject(StateControlService);
   activeButton: boolean = false
 
   closeAddUsers() {
@@ -22,5 +24,9 @@ export class AddUsersComponent {
   }
   onButtonChanged(value: boolean) {
     this.activeButton = value;
+  }
+  async addUserToChat() {
+    await this.chat.updateSpecificPeopleInChannelFromState();
+    this.closeAddUsers()
   }
 }
