f3e8ed9ac6399310f45540c3efc2ec03858541e3
Damian Poloczek <damianpoloczek@gmail.com>
2024-12-08 00:35:17 +0100

    confirm-leave-channel structure

--- src/app/main-content/chat-room/confirm-leave-channel/confirm-leave-channel.component.ts
+++ src/app/main-content/chat-room/confirm-leave-channel/confirm-leave-channel.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CloseComponent } from '../../../shared/component/close/close.component';
+import { MatDialogRef } from '@angular/material/dialog';
+import { StateControlService } from '../../../services/state-control/state-control.service';
+import { FirebaseService } from '../../../services/firebase/firebase.service';
 
 @Component({
-  selector: 'app-confirm-leave-channel',
+  selector: 'app-confirm-delete-channel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, CloseComponent],
   templateUrl: './confirm-leave-channel.component.html',
-  styleUrl: './confirm-leave-channel.component.scss'
+  styleUrl: './confirm-leave-channel.component.scss',
 })
 export class ConfirmLeaveChannelComponent {
+  readonly dialog = inject(MatDialogRef<ConfirmLeaveChannelComponent>);
+  stateControl = inject(StateControlService);
+  fb = inject(FirebaseService);
 
+  constructor(
+    public confirmDialogRef: MatDialogRef<ConfirmLeaveChannelComponent>
+  ) {}
+
+  closeLeaveChannel() {
+    this.dialog.close();
+  }
+
+  confirmLeaveChannel() {
+    const result = true;
+    this.confirmDialogRef.close(result);
+  }
 }
