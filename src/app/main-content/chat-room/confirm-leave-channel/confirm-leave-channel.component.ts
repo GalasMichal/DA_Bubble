e6b24171ba562776d59480f69d50364373ce15c4
Damian Poloczek <damianpoloczek@gmail.com>
2024-12-07 23:58:10 +0100

    new component confirm-leave-channel

---
+++ src/app/main-content/chat-room/confirm-leave-channel/confirm-leave-channel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-leave-channel',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-leave-channel.component.html',
+  styleUrl: './confirm-leave-channel.component.scss'
+})
+export class ConfirmLeaveChannelComponent {
+
+}
