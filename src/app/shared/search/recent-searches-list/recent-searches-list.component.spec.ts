4ed6e7f5124d3dd97cb4b0c1242e4fd4054ff1e5
Damian Poloczek <damianpoloczek@gmail.com>
2024-12-23 23:37:09 +0100

    next component recent-searches-list

---
+++ src/app/shared/search/recent-searches-list/recent-searches-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecentSearchesListComponent } from './recent-searches-list.component';
+
+describe('RecentSearchesListComponent', () => {
+  let component: RecentSearchesListComponent;
+  let fixture: ComponentFixture<RecentSearchesListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RecentSearchesListComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(RecentSearchesListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/search/recent-searches-list/recent-searches-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-searches-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './recent-searches-list.component.html',
+  styleUrl: './recent-searches-list.component.scss'
+})
+export class RecentSearchesListComponent {
+
+}
