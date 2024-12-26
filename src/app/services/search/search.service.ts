726a875be88ca0353cdd7d917753227f8788b5da
Damian Poloczek <damianpoloczek@gmail.com>
2024-12-26 21:58:17 +0100

    channels works in search input

---
+++ src/app/services/search/search.service.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  public readonly searchTerm = signal('');
+
+  router = inject(Router);
+
+  constructor() { }
+
+  clearSearch() {
+    this.searchTerm.set('');
+  }
+
+  search() {
+    this.router.navigate([`/search/${this.searchTerm()}`]);
+    // this.recentSearchesService.addToRecentSearches(this.searchTerm());
+  }
+}
--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
+import { SearchService } from '../../services/search/search.service';
+import { ChatRoomService } from '../../services/chat-room/chat-room.service';
+import { StateControlService } from '../../services/state-control/state-control.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  searchService = inject(SearchService);
+  searchTerm = this.searchService.searchTerm;
+  stateControl = inject(StateControlService)
+  chat = inject(ChatRoomService);
+
+  searchInput = viewChild.required<ElementRef<HTMLInputElement>>('searchInput');
+
+  clearSearch() {
+    this.searchService.clearSearch();
+  }
+
+  openChannel(chanId: string) {
+    this.stateControl.responsiveChat = true;
+    this.stateControl.responsiveArrow = true;
+    this.stateControl.responsiveMenu = true;
+    this.stateControl.isThreadOpen = false
+    this.chat.openChatById(chanId);
+  }
+
+  search() {
+    if (!this.searchTerm()) {
+      return;
+    }
+
+    this.searchService.search();
+  }
 
 }
