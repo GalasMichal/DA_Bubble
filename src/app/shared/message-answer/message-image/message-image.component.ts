28b1a76b3e684a1389d68847860259d64067a394
Damian Poloczek <damianpoloczek@gmail.com>
2025-01-06 21:12:55 +0100

    new c message-image

---
+++ src/app/shared/message-answer/message-image/message-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-image.component.html',
+  styleUrl: './message-image.component.scss'
+})
+export class MessageImageComponent {
+
+}
