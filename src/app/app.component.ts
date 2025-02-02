15c9d97df13222c754fb4dec55e44ba8f81982d6
Damian Poloczek <damianpoloczek@gmail.com>
2025-02-02 19:08:01 +0100

    pre-loader-small

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -22,13 +22,13 @@ import { FirebaseService } from './services/firebase/firebase.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dabubble';
   stateControl = inject(StateControlService)
-  loading = signal(true); // Using Angular's signal API
+  loading = signal(true);
   fb = inject(FirebaseService)
 
-  constructor() {
+  ngOnInit(): void {
     setTimeout(() => {
       this.loading.set(false);
     }, 2000);
