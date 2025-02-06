570380a69240c81ec465074f1a7b4d08653e4ed8
Damian Poloczek <damianpoloczek@gmail.com>
2025-02-06 19:44:17 +0100

    remove some code

--- src/app/main-content/chat-room/channel-create/channel-create.component.ts
+++ src/app/main-content/chat-room/channel-create/channel-create.component.ts
@@ -3,7 +3,6 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import {
   MatDialog,
-  MatDialogContent,
   MatDialogRef,
 } from '@angular/material/dialog';
 
@@ -22,7 +21,6 @@ import { ChatRoomService } from '../../../services/chat-room/chat-room.service';
 import { FirebaseService } from '../../../services/firebase/firebase.service';
 import { StateControlService } from '../../../services/state-control/state-control.service';
 import { UserServiceService } from '../../../services/user-service/user-service.service';
-import { User } from '../../../models/interfaces/user.model';
 
 @Component({
   selector: 'app-channel-create',
