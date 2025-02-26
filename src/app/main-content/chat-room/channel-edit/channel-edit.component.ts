b3a94fff360e86dd3300d1017ec73beff1e81549
Michal Galas <michalgalas1989@gmail.com>
2025-02-26 12:54:36 +0100

    code refactoring in channel edit

--- src/app/main-content/chat-room/channel-edit/channel-edit.component.ts
+++ src/app/main-content/chat-room/channel-edit/channel-edit.component.ts
@@ -1,12 +1,9 @@
-import { Component, computed, inject, Input, input, signal, Signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CloseComponent } from '../../../shared/component/close/close.component';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { ChatRoomService } from '../../../services/chat-room/chat-room.service';
-import { Channel } from '../../../models/interfaces/channel.model';
 import { FormsModule } from '@angular/forms';
-import { Firestore } from '@angular/fire/firestore';
-import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { StateControlService } from '../../../services/state-control/state-control.service';
 import { ToastComponent } from '../../../shared/component/toast/toast.component';
 import { ConfirmDeleteChannelComponent } from '../confirm-delete-channel/confirm-delete-channel.component';
@@ -15,6 +12,7 @@ import { DialogGlobalComponent } from '../../../shared/component/dialog-global/d
 import { Router } from '@angular/router';
 import { ConfirmLeaveChannelComponent } from '../confirm-leave-channel/confirm-leave-channel.component';
 import { UserServiceService } from '../../../services/user-service/user-service.service';
+import { User } from '../../../models/interfaces/user.model';
 
 @Component({
   selector: 'app-channel-edit',
@@ -24,17 +22,29 @@ import { UserServiceService } from '../../../services/user-service/user-service.
   styleUrl: './channel-edit.component.scss',
 })
 export class ChannelEditComponent {
+  /**
+   * Inject the MatDialogRef service to close the dialog
+   * Inject the StateControlService to access the global state
+   * Inject the MatDialog service to open the confirm dialog
+   * Inject the ChatRoomService to access the chat room
+   * Inject the FirebaseService to access the firebase
+   * Inject the UserServiceService to access the user list
+   * Inject the Router to navigate to another page
+   */
   readonly dialog = inject(MatDialogRef<ChannelEditComponent>);
   dialogConfirm = inject(MatDialog);
-  // isDisabled: boolean = false;
-  channelEditTitel: boolean = false;
-  channelEditDescription: boolean = false;
   chat = inject(ChatRoomService);
-  firestore = inject(Firestore);
   stateServer = inject(StateControlService);
   userService = inject(UserServiceService);
   fb = inject(FirebaseService);
   router = inject(Router);
+
+  channelEditTitel: boolean = false;
+  channelEditDescription: boolean = false;
+
+  /**
+   * Get the current channel from the chat room service signal
+   */
   currentChannel = computed(() => this.chat.currentChannelSignal());
 
   channelName = this.currentChannel()?.channelName;
@@ -42,20 +52,34 @@ export class ChannelEditComponent {
   newTitle: string = '';
   newDescription: string = '';
   counter: number = 0;
-  isDisabled = this.currentChannel()?.createdBy[0].uId !== this.fb.currentUser()?.uId;
+  /**
+   * Check if the user is the creator of the channel
+   */
+  isDisabled =
+    this.currentChannel()?.createdBy[0].uId !== this.fb.currentUser()?.uId;
   isDisabledCreatedBy =
-    this.currentChannel()?.createdBy[0].uId ===
-    this.fb.currentUser()?.uId;
+    this.currentChannel()?.createdBy[0].uId === this.fb.currentUser()?.uId;
 
+  /**
+   * Filter all users in the channel
+   */
   constructor() {
     if (this.stateServer.createChannelActiveInput) {
-      this.filterAllUsersInChannel()
+      this.filterAllUsersInChannel();
     }
   }
 
+  /**
+   * Close the dialog
+   */
   closeChannelEdit() {
     this.dialog.close();
   }
+
+  /**
+   * Handle the counter to show the dialog
+   * If the counter is greater than 2, show the dialog
+   */
   onCounter() {
     if (this.counter >= 2) {
       this.showDialog();
@@ -63,6 +87,9 @@ export class ChannelEditComponent {
     }
   }
 
+  /**
+   * Edit the channel tittle
+   */
   onEditTittle() {
     this.counter++;
 
@@ -73,6 +100,9 @@ export class ChannelEditComponent {
     }
   }
 
+  /**
+   * Edit the channel description
+   */
   onEditDescription() {
     this.counter++;
 
@@ -83,30 +113,57 @@ export class ChannelEditComponent {
     }
   }
 
+  /**
+   * Show the dialog
+   */
   showDialog() {
     this.dialogConfirm.open(DialogGlobalComponent, {
       panelClass: 'dialog-global-container',
     });
   }
 
+  /**
+   * Edit the channel tittle
+   */
   editChannelTittle() {
     this.channelEditTitel = !this.channelEditTitel;
   }
 
+  /**
+   * Save the channel tittle
+   */
   saveChannelTittle() {
     this.channelEditTitel = !this.channelEditTitel;
     this.newTitle = this.currentChannel()!.channelName;
   }
 
+  /**
+   * Edit the channel description
+   */
   editChannelDescription() {
     this.channelEditDescription = !this.channelEditDescription;
   }
 
+  /**
+   * Save the channel description
+   */
   saveChannelDescription() {
+    this.toggleChannelEditDescription();
+    this.setNewDescription();
+  }
+
+  toggleChannelEditDescription() {
     this.channelEditDescription = !this.channelEditDescription;
+  }
+
+  setNewDescription() {
     this.newDescription = this.currentChannel()!.channelDescription;
   }
 
+  /**
+   *  Update the channel
+   * @param text text to show in the toast
+   */
   onUpdateChannel(text: string) {
     this.counter++;
 
@@ -118,14 +175,41 @@ export class ChannelEditComponent {
   }
 
   updateChannel(text: string) {
-    const newName = this.newTitle === '' ? this.currentChannel()!.channelName : this.newTitle;
-    const newDescription = this.newDescription === '' ? this.currentChannel()!.channelDescription: this.newDescription;
-    this.currentChannel()!.channelName = newName;
-    this.currentChannel()!.channelDescription = newDescription;
-    this.chat.updateChannel(this.currentChannel()!)
+    const newName = this.getNewChannelName();
+    const newDescription = this.getNewChannelDescription();
+    this.applyChannelUpdates(newName, newDescription, text);
+  }
+
+  getNewChannelName() {
+    return this.newTitle === ''
+      ? this.currentChannel()!.channelName
+      : this.newTitle;
+  }
+
+  getNewChannelDescription() {
+    return this.newDescription === ''
+      ? this.currentChannel()!.channelDescription
+      : this.newDescription;
+  }
+
+  applyChannelUpdates(newName: string, newDescription: string, text: string) {
+    const currentChannel = this.currentChannel();
+    if (currentChannel) {
+      currentChannel.channelName = newName;
+      currentChannel.channelDescription = newDescription;
+      this.chat.updateChannel(currentChannel);
+      this.showUpdateToast(text);
+      this.closeDialogAfterDelay();
+    }
+  }
+
+  showUpdateToast(text: string) {
     this.stateServer.showToast = true;
     this.stateServer.showToastText.set(text);
     this.stateServer.removeShowToast();
+  }
+
+  closeDialogAfterDelay() {
     setTimeout(() => {
       this.dialog.close();
     }, 2200);
@@ -137,27 +221,32 @@ export class ChannelEditComponent {
     if (this.isDisabled) {
       this.onCounter();
     } else {
-      this.deleteChannel(chanId);
+      this.confirmAndDeleteChannel(chanId);
     }
   }
 
-  deleteChannel(chanId: string) {
-    const confirmDialogRef = this.dialogConfirm.open(
-      ConfirmDeleteChannelComponent,
-      {
-        panelClass: 'confirm-delete-channel',
-      }
-    );
+  confirmAndDeleteChannel(chanId: string) {
+    const confirmDialogRef = this.openConfirmDialog();
 
     confirmDialogRef.afterClosed().subscribe((result: boolean) => {
       if (result) {
-        this.chat.deleteChannel(chanId)
-        this.dialogConfirm.closeAll();
-        this.router.navigate(['main']);
+        this.performChannelDeletion(chanId);
       } else {
         confirmDialogRef.close();
       }
     });
+  }
+
+  openConfirmDialog() {
+    return this.dialogConfirm.open(ConfirmDeleteChannelComponent, {
+      panelClass: 'confirm-delete-channel',
+    });
+  }
+
+  performChannelDeletion(chanId: string) {
+    this.chat.deleteChannel(chanId);
+    this.dialogConfirm.closeAll();
+    this.router.navigate(['main']);
   }
 
   // Show all users except this user which created this channel
@@ -167,32 +256,48 @@ export class ChannelEditComponent {
     const showAllChoosenUsers = currentChannel?.specificPeople; // Array of user IDs
     const allUsers = this.userService.userList; // Array of User objects
 
-    const filteredUsers = allUsers.filter(
-      (user) => showAllChoosenUsers?.includes(user.uId) && user.uId);
+    const filteredUsers = this.getFilteredUsers(allUsers, showAllChoosenUsers);
     this.stateServer.choosenUser = filteredUsers; // Assign filtered users
-}
+  }
+
+  getFilteredUsers(
+    allUsers: User[],
+    showAllChoosenUsers: string[] | undefined
+  ) {
+    return allUsers.filter(
+      (user) => showAllChoosenUsers?.includes(user.uId) && user.uId
+    );
+  }
 
   leaveChannel() {
     const currentUser = this.fb.currentUser();
+    this.confirmAndLeaveChannel(currentUser);
+  }
 
-    const confirmLeaveDialog = this.dialogConfirm.open(
-      ConfirmLeaveChannelComponent,
-      {
-        panelClass: 'confirm-leave-channel',
-      }
-    );
+  confirmAndLeaveChannel(currentUser: User | null) {
+    const confirmLeaveDialog = this.openLeaveConfirmDialog();
+
     confirmLeaveDialog.afterClosed().subscribe((result: boolean) => {
       if (result) {
-        this.stateServer.choosenUser =
-          this.stateServer.choosenUser.filter(
-            (user) => user.uId !== currentUser!.uId
-          );
-        // this.chat.updateSpecificPeopleInChannelFromState();
-        this.dialogConfirm.closeAll();
-        this.router.navigate(['main']);
+        this.performChannelLeave(currentUser);
       } else {
         confirmLeaveDialog.close();
       }
     });
+  }
+
+  openLeaveConfirmDialog() {
+    return this.dialogConfirm.open(ConfirmLeaveChannelComponent, {
+      panelClass: 'confirm-leave-channel',
+    });
+  }
+
+  performChannelLeave(currentUser: User | null) {
+    this.stateServer.choosenUser = this.stateServer.choosenUser.filter(
+      (user) => user.uId !== currentUser!.uId
+    );
+    // this.chat.updateSpecificPeopleInChannelFromState();
+    this.dialogConfirm.closeAll();
+    this.router.navigate(['main']);
   }
 }
