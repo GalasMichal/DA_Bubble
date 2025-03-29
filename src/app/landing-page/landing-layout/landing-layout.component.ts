af8152d1fe89ad242490ac735107141dc900f56d
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-29 23:14:03 +0100

    intro ready

--- src/app/landing-page/landing-layout/landing-layout.component.ts
+++ src/app/landing-page/landing-layout/landing-layout.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderLandingComponent } from '../header-landing/header-landing.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -15,4 +15,16 @@ import { ToastComponent } from '../../shared/component/toast/toast.component';
 })
 export class LandingLayoutComponent {
   stateControl = inject(StateControlService)
+
+
+  showIntro: boolean = false;
+
+  constructor() {
+    const showIntro = JSON.parse(localStorage.getItem("showIntro") || "null");
+    if(!showIntro) {
+      localStorage.setItem("showIntro", "true");
+    } else {
+      this.showIntro = showIntro
+    }
+  }
 }
